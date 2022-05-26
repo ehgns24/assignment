93098c1e6b1bb70d43c049621ed1534eab440257
dohunkwon <ehgns24@naver.com>
2022-05-26 15:04:56 +0900

    3주차 과제 제출용

---
+++ src/main/java/com/sparta/assignment/domain/MemoRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.assignment.domain;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String title;
+    private String contents;
+    private String password;
+}
